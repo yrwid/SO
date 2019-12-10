6593fc80a836455ae07bace9c05bd925730db37e
yrwid <mudri656@gmail.com>
2019-12-10 21:17:35 +0100

    added final version linux so ringdev Buff

--- Linux/ringdev.c
+++ Linux/ringdev.c
@@ -30,79 +30,62 @@ static struct mutex ringdev_lock;
  */
 static char ringdev_buf[4096];
 static size_t ringdev_len;
-static int writePtr;
-//static int *readPtr;
+static int writePtr;						//Global writePtr
+static _Bool backToZero = 0;					//Global variable thats indiacate if  writePtr == readPtr, but  buffor makes one full round
 static DECLARE_WAIT_QUEUE_HEAD( headQue );
-static _Bool flag = 0;
-static int bufforFull =0;//, in_flag =0;
 
 static int ringdev_open(struct inode *inode, struct file *filp)
 {
-//	printk( KERN_INFO "open \n" );
 	int*  zm;
 	zm = kmalloc(sizeof(int),GFP_KERNEL);
+	if(!zm)  						//Detected kmallock error  
+	{
+		return -ENOMEM;
+	}
 	*zm = writePtr;
 	filp->private_data = zm;
-	//filp->private_data = (int)0;
-	
 	return 0;
 }
 
 static ssize_t ringdev_read(struct file *filp, char __user *buf, size_t count,loff_t* off)
 { 
-	ssize_t ret = 0;
+	ssize_t ret = 0,wakeUp = 0 ; 				//Return and wakeUp singla  varaibles 
 	int *readPtr;
 
 	mutex_lock(&ringdev_lock);
 	readPtr = (int*)filp->private_data;
-	pr_info("readPtr: %d", (*readPtr));
-	pr_info("info");
+
+//Check again after wake up
 check_again:
-//	pr_info("\n readPtr: %zd, writePtr %zd \n",readPtr, writePtr );
+
  	if(writePtr < *readPtr)
 	{
-		if(bufforFull == 1)
-		{
-			count = ringdev_len - *readPtr;
-			//readPtr = 0;
-		}
-		else
-			return 0;
+		count = ringdev_len - *readPtr;
+		backToZero = 0;
 	}
-//	pr_info("count: %zd \n ", count);
-
-//	ssize_t ret = 0;
-
-	//mutex_lock(&ringdev_lock);
-
-	if (*readPtr >= ringdev_len)
+	else if(writePtr  >  *readPtr)
 	{
-		count = 0;
-	}
-	else if (writePtr  >=  *readPtr)
-	{
-
 		count = writePtr - *readPtr;
+		backToZero = 0;
 	}
-
-			if(count == 0)
-			{
-				mutex_unlock(&ringdev_lock);
-				ret = wait_event_interruptible( headQue ,writePtr != *readPtr);
-  		   	  //	pr_info("\n return val: %zd \n", ret);
- 			  //	pr_info("readPtr: %zd \n", readPtr);
-  			  //	pr_info("writePtr: %zd \n", writePtr);
-				if(ret != 0)
-				{
-					 return -ERESTARTSYS;
-				}
-				mutex_lock(&ringdev_lock);
-				flag = 0;
-				goto check_again;
-			}
+	else if(writePtr  ==  *readPtr && !backToZero  )
+	{
+		mutex_unlock(&ringdev_lock);
+		wakeUp  = wait_event_interruptible( headQue ,(writePtr != *readPtr) || backToZero );
+		if(wakeUp != 0)
+		{
+			 return -ERESTARTSYS;
+		}
+		mutex_lock(&ringdev_lock);
+		goto check_again;
+	}
+ 	else if(writePtr == *readPtr && backToZero)
+	{
+		count = ringdev_len;
+		backToZero = 0;
+	}
 
 	ret = -EFAULT;
-//	pr_info("read_Ptr = %d, count: %zd\n", readPtr, count);
 	if (copy_to_user(buf,&ringdev_buf[*readPtr], count))
 	{
 		goto out_unlock;
@@ -110,73 +93,53 @@ check_again:
 
 	ret = count;
 	*readPtr += count;
-	if(*readPtr >= ringdev_len)
+	if(*readPtr >= ringdev_len)				//Looking for buffor  overflow 
 	{
 		*readPtr = 0;
-//                return -EFAULT;
 	}
+
 out_unlock:
 	mutex_unlock(&ringdev_lock);
-//	pr_info("return val: %zd\n",ret);
 	return ret;
 }
 
 static ssize_t ringdev_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *off)
 {
-	ssize_t ret = 0;
+	ssize_t ret = 0;					//Return value 
 	mutex_lock(&ringdev_lock);
-	pr_info("/n");
-	//pr_info(" START: writePtr: %d readPtr: %d", writePtr,readPtr);
-	//przepełnienie 
-	pr_info("przed 1 if");
-	if(writePtr + count  >= ringdev_len - 1)
+
+	if(writePtr + count  >= ringdev_len)			//Overflow detected
 	{
-		pr_info("w 1 ifie");
 		count = ringdev_len - writePtr;
-		bufforFull = 1;
-		//count = 0;
+
 	}
-//	pr_info("2 if");
-	else if(count >= ringdev_len)
-	{
-		pr_info(" w 2 if");
-		count = ringdev_len;
-	}
-//	else if(writePtr + count > ringdev_len - 1 )
-//	{
-//		count = ringdev_len - writePtr - 1;
-//		in_flag = 1;
-//	}
-	pr_info("przed copy");
-	//pr_info("PRZED COPY:  writePtr: %d readPtr: %d", writePtr, readPtr);
+
 	if(copy_from_user(&ringdev_buf[writePtr], buf , count))
 	{
-		pr_info("w bledzzie copy_from_user");
-		printk(KERN_INFO "copy_from_user not null err\n");
+		printk(KERN_INFO "copy_from_user  err\n");
 		mutex_unlock(&ringdev_lock);
 		return -EFAULT;
 	}
 	ret = count;
-//	*off +=  ret;
 	writePtr += count;
-	pr_info("przed wyzerowaniem");
-	//pr_info("PRZED WTZEROWANIEM: writePtr: %d readPtr: %d",writePtr, readPtr);
-	if(writePtr >= ringdev_len - 1)
+	if(writePtr >= ringdev_len)				//Looking for buffor overflow 
 	{
 		writePtr = 0;
+		backToZero = 1 ;
+		wake_up_interruptible(&headQue);
+		mutex_unlock(&ringdev_lock);
+		return -EPIPE;					//Return broken PIPE error
 	}
-	flag = 1;
-	wake_up_interruptible(&headQue);
-	
-	pr_info("PO obudzeniu: return: %zd", ret);
+	wake_up_interruptible(&headQue);			//Wake up read function 
+
 	mutex_unlock(&ringdev_lock);
 	return ret; 
 }
 
 static int ringdev_release(struct inode *inode, struct file *filp)
 {
-	kfree(filp->private_data);
+	kfree(filp->private_data);				//Realeas allocated memory 
 	return 0;
 }
 
@@ -199,15 +162,12 @@ static int __init ringdev_init(void)
 	int ret;
 
 	mutex_init(&ringdev_lock);
-//	ringdev_len = snprintf(ringdev_buf, sizeof(ringdev_buf),
-//			"Hello world!\n");
 	ret = misc_register(&ringdev_miscdevice);
 	if (ret < 0) {
 		pr_err("can't register miscdevice.\n");
 		return ret;
 	}
-	//readPtr = 0;
-	writePtr = 0;
+	writePtr = 0;					//Set writePtr to initial value = 0;
 	pr_info("minor %d\n", ringdev_miscdevice.minor);
 	ringdev_len = 4096;
 	return 0;
